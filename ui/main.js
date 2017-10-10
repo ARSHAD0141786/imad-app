de61a0208a845ec92ddb43d4bd6b1e9a01758ebe
ARSHAD0141786 <arshad0141786@gmail.com>
2017-10-10 22:43:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,113 +1,75 @@
-console.log('Loaded!');
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+var Pool = require('pg').Pool;
+var crypto = require('crypto');
+var bodyParser = require('body-parser');
+var config={
+  user:'arshadmohammed0141',
+  database:'arshadmohammed0141',
+  host:'db.imad.hasura-app.io',
+  port:'5432',
+  password:process.env.DB_PASSWORD
+};
+var pool = new Pool(config);
 
+var app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
 
-
-function counter_inc() {
-	//Create a request
-	console.log("Counter function invoked");
-	var req=new XMLHttpRequest();
-
-	//Capture the response and store it in a variable
-	req.onreadystatechange = function()
-	{
-		if(req.readyState === XMLHttpRequest.DONE){
-			//Take some action
-			if(req.status === 200){
-				var span1=document.getElementById('counter');
-				span1.innerHTML=req.responseText;
-			}
-		}
-	}
-
-
-	//Make a request
-	req.open('GET','http://localhost:80/counter_increment',true);
-	req.send(null);
-
-	// count = count + 1
-	// var counter = document.getElementById('counter');
-	// counter.innerHTML = count.toString();
+function hash(input,salt){
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
 }
 
-function counter_disp() {
-	//Create a request
-	console.log("Counter function invoked");
-	var req=new XMLHttpRequest();
+app.post('/create-user',function(req,res){
+    var username = req.body.username;
+    var password = req.body.password;
+    var salt = crypto.randomBytes(128).toString('hex');
+    var dbString = hash(password,salt);
+    pool.query('INSERT INTO "user123" (user_id,password_string) VALUES ($1,$2)',[username,dbString],function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            res.send('User succcessfully created : '+username);
+        }
+    });
+});
 
-	//Capture the response and store it in a variable
-	req.onreadystatechange = function()
-	{
-		if(req.readyState === XMLHttpRequest.DONE){
-			//Take some action
-			if(req.status === 200){
-				var span1=document.getElementById('counter');
-				span1.innerHTML=req.responseText;
-			}
-		}
-	}
+app.get('/hash/:input',function(req,res){
+    var hashedString=hash(req.params.input,'this-is-some-random-string');
+    res.send(hashedString);
+});
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 
-	//Make a request
-	req.open('GET','http://localhost:80/counter_display',true);
-	req.send(null);
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
 
-	// count = count + 1
-	// var counter = document.getElementById('counter');
-	// counter.innerHTML = count.toString();
-}
-
-counter_disp();
-
-var bt=document.getElementById('count');
-bt.onclick=function (){
-	counter_inc();
-}
-// submit names by using js no request is sent on the server
-/*
-var nameInput=document.getElementById('naam');
-var names=[];
-var sub=document.getElementById('submit');
-sub.onclick=function(){
-	var list ='';
-	names.push(nameInput.value);
-	for(var i=0;i<names.length;i++)
-	{
-		list += '<li>' + names[i] + '</li>';
-	}
-	var ul=document.getElementById('list');
-	ul.innerHTML=list;
-}
-*/
-
-var nameInput=document.getElementById('naam');
-var sub=document.getElementById('submit');
-sub.onclick=function(){
-		//Create a request
-	var req=new XMLHttpRequest();
-
-	//Capture the response and store it in a variable
-	req.onreadystatechange = function()
-	{
-		if(req.readyState === XMLHttpRequest.DONE){
-			//Take some action
-			if(req.status === 200){
-				var list ='';
-				var names=req.responseText;
-				names = JSON.parse(names);
-				for(var i=0;i<names.length;i++)
-				{
-					list += '<li>' + names[i] + '</li>';
-				}
-				var ul=document.getElementById('list');
-				ul.innerHTML=list;
-			}
-		}
-	}
+app.get('/test-db',function(req,res){
+    //make a select request
+    //retrun a response with the results
+    pool.query('SELECT * FROM user123;',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send(JSON.stringify(result.rows));
+        }
+    });
+});
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
 
 
-	//Make a request
-	req.open('GET','http://localhost:80/submit-name?name=' + nameInput.value,true);
-	req.send(null);
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
 
-	
-}
+var port = 80;
+app.listen(port, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});

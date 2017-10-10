82afa773f89ff64c153464725bee826e874e92ef
ARSHAD0141786 <arshad0141786@gmail.com>
2017-10-10 19:02:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 
 function hash(input,salt){
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return hashed.toString('hex');
+    return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
 }
 
 app.post('/create-user',function(req,res){

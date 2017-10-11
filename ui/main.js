66f3b39e727fe1ec5bc4aa879f45f9d5dcb9a9fb
ARSHAD0141786 <arshad0141786@gmail.com>
2017-10-11 20:27:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -100,8 +100,8 @@ submit_bt.onclick=function(){
     };
     
     
-    var username=document.getElementById('username');
-    var password=document.getElementById('password');
+    var username=document.getElementById('username').value;
+    var password=document.getElementById('password').value;
     console.log(username);
     console.log(password);
     

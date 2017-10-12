73068bb8fcb4d402a1cc07894c4867d66f3ea799
ARSHAD0141786 <arshad0141786@gmail.com>
2017-10-12 17:12:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -40,15 +40,14 @@ app.post('/login',function(req,res){
     var username = req.body.username;
     var password = req.body.password;
     
-    pool.query('SELECT *FROM "user123" WHERE username = $1',[username],function(err,result){
+    pool.query('SELECT *FROM "user123" username = $1',[username],function(err,result){
         if(err){
-            res.status(500).send(err.toString()+'server ProBlem');
+            res.status(500).send(err.toString());
         }else{
             if(result.rows.length===0){
                 res.status(403).send('username/password is invalid');
             }else{
                 //match password
-                console.log('username found but password cannot be fetched.');
                 var dbString=result.rows[0].password;
                 var salt=dbString.split('$')[2];
                 var hashedPass=hash(password,salt);

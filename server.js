b90bf1bd600efbc7a0161782a4d6cbe5863a1eb4
ARSHAD0141786 <arshad0141786@gmail.com>
2017-10-13 00:34:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,7 +75,6 @@ app.post('/login',function(req,res){
 
 
 app.get('/check-login',function(req,res){
-    res.send(req.session.auth.userId.toString());
     if(req.session && req.session.auth && req.session.auth.userId){
         res.send('You are logged in  : ' + req.session.auth.userId.toString());
     }else{

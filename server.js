1135846616f7aa0090c2cca3da22a8841e543518
ARSHAD0141786 <arshad0141786@gmail.com>
2017-10-15 10:03:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.post('/login',function(req,res){
                     //set cookie with a session internally on the server side it maps the session id 
                     //to an object {auth:{userId}}
                     
-                    res.send("SignIn successfully");
+                    res.send(JSON.stringify({"SignIn" :"Successfully"}));
                 }else{
                     res.status(403).send('username is found but password not matched.');
                 }

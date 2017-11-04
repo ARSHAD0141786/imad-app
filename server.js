64c5c9e6f56abdb17554ebb52dfa4fd906ecf318
ARSHAD0141786 <arshad0141786@gmail.com>
2017-11-04 23:52:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -160,11 +160,9 @@ app.post('/send-feedback-for-messes',function(req,res){
             var users = result.rows[0].users;
             
             
-            
-            
             var updatedRatingFood = (ratingFood * users + foodRating)/(users + 1);
             var updatedRatingCleaning = (ratingCleaning * users + cleaningRating)/(users + 1);
-            var updatedUsers = users + 1;
+            var updatedUsers = users++;
             
             console.log("Values : kasd;l  "+updatedRatingFood+" "+updatedRatingCleaning+" "+updatedUsers);
             
